0dfcc8c6e5552b722849ba1fee656afc38ba7098
Bahriddin <abahriddin67@gmail.com>
2024-11-20 23:13:32 +0500

    2a

--- src/main/java/lesson/uz/HelloController.java
+++ src/main/java/lesson/uz/HelloController.java
@@ -3,6 +3,7 @@ package lesson.uz;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
