313255b4af9cac6e5af79f6670c9fe2bf8dd2767
Abdullayev Bahriddin <120470158+BaxriddinAbdullayev@users.noreply.github.com>
2024-11-20 23:07:46 +0500

    Update HelloController.java

--- src/main/java/lesson/uz/HelloController.java
+++ src/main/java/lesson/uz/HelloController.java
@@ -2,6 +2,7 @@ package lesson.uz;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
